bbec5307ea475ec708428dabc6e10a6610b56613
Filia Naura Widyanggari <filianaura07@gmail.com>
2026-09-19 11:13:43 +0700

    initial commit

---
+++ src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        System.out.println("aku suka kamu");
+        System.out.println("bahlil jelek");
+        System.out.println("ini hari sabtu");
+    }
+}
